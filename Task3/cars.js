5de1fd007ea627e9f40c996dca6b7770caedcd2b
YegorFeoktistov <YegorFeoktistov@gmail.com>
2018-04-10 23:30:07 +0300

    Remove RegExps, change validation, add custom errors, and other changes

--- Task3/cars.js
+++ Task3/cars.js
@@ -1,3 +1,4 @@
+//#region
 /*
 	Functions for making private fields
 */
@@ -45,12 +46,37 @@ function unlockObjectField(object, field) {
 	});
 }
 
+//#endregion
+
+/*
+	Validation function
+*/
+
+const isNumeric = (n) => { return (!isNaN(parseFloat(n)) && isFinite(n)); };
+
+/*
+	Custom exceptions declaration
+*/
+
+const InvalidNumberError = new Error("Numeric value is expected. Number must be greater than 0.");
+const InvalidStringError = new Error("String value is expected. Value must not be empty.");
+
+//#region
 /*
 	Classes declaration
 */
 
-let Car = {
-	constructor: function(model, number, owner, weight, maxSpeed, mileage, wheelsAmount, yearOfAssembly) {
+const Car = {
+	constructor: function(
+		model,
+		number,
+		owner,
+		weight,
+		maxSpeed,
+		mileage,
+		wheelsAmount,
+		yearOfAssembly
+	) {
 		this._model = model || "unknown model";
 		this._number = number || "unknown number";
 		this._owner = owner || "unknown owner";
@@ -67,7 +93,12 @@ let Car = {
 	},
 	set model(value) {
 		unlockObjectField(this, "_model");
-		this._model = !value ? this._model : /\W/.test(value) ? this._model : /\d/.test(value) ? this._model : value;
+		if (!value) {
+			throw InvalidStringError;
+		}
+		else {
+			this._model = value + "";
+		}
 		lockObjectField(this, "_model");
 	},
 
@@ -76,7 +107,12 @@ let Car = {
 	},
 	set number(value) {
 		unlockObjectField(this, "_number");
-		this._number = !value ? this._number : /\W/.test(value) ? this._number : value.toString();
+		if (!value) {
+			throw InvalidStringError;
+		}
+		else {
+			this._number = value + "";
+		}
 		lockObjectField(this, "_number");
 	},
 
@@ -85,7 +121,12 @@ let Car = {
 	},
 	set owner(value) {
 		unlockObjectField(this, "_owner");
-		this._owner = !value ? this._owner : /\d/.test(value) ? this._owner : value;
+		if (!value) {
+			throw InvalidStringError;
+		}
+		else {
+			this._owner = value + "";
+		}
 		lockObjectField(this, "_owner");
 	},
 
@@ -94,7 +135,12 @@ let Car = {
 	},
 	set weight(value) {
 		unlockObjectField(this, "_weight");
-		this._weight = !value ? this._weight : isNaN(+value) ? this._weight : +value;
+		if (isNumeric(value) && value > 0) {
+			this._weight = +value;
+		}
+		else {
+			throw InvalidNumberError;
+		}
 		lockObjectField(this, "_weight");
 	},
 
@@ -103,7 +149,12 @@ let Car = {
 	},
 	set maxSpeed(value) {
 		unlockObjectField(this, "_maxSpeed");
-		this._maxSpeed = !value ? this._maxSpeed : isNaN(+value) ? this._maxSpeed : +value;
+		if (isNumeric(value) && value > 0) {
+			this._maxSpeed = +value;
+		}
+		else {
+			throw InvalidNumberError;
+		}
 		lockObjectField(this, "_maxSpeed");
 	},
 
@@ -112,7 +163,12 @@ let Car = {
 	},
 	set mileage(value) {
 		unlockObjectField(this, "_mileage");
-		this._mileage = !value ? this._mileage : isNaN(+value) ? this._mileage : +value;
+		if (isNumeric(value) && value > 0) {
+			this._mileage = +value;
+		}
+		else {
+			throw InvalidNumberError;
+		}
 		lockObjectField(this, "_mileage");
 	},
 
@@ -121,7 +177,12 @@ let Car = {
 	},
 	set wheelsAmount(value) {
 		unlockObjectField(this, "_wheelsAmount");
-		this._wheelsAmount = !value ? this._wheelsAmount : isNaN(+value) ? this._wheelsAmount : /\d{3,}/.test(value) ? this._wheelsAmount : +value;
+		if (isNumeric(value) && value > 0) {
+			this._wheelsAmount = +value > 99 ? 99 : +value;
+		}
+		else {
+			throw InvalidNumberError;
+		}
 		lockObjectField(this, "_wheelsAmount");
 	},
 
@@ -130,16 +191,18 @@ let Car = {
 	},
 	set yearOfAssembly(value) {
 		unlockObjectField(this, "_yearOfAssembly");
-		this._yearOfAssembly = !value ? this._yearOfAssembly :
-		/\D/.test(value) ? this._yearOfAssembly :
-		/\d{5,}/.test(value) ? this._yearOfAssembly :
-		/\d{4}/.test(value) ? value.toString() : this._yearOfAssembly;
+		if (isNumeric(value) && value > 1000 && value <= 9999) {
+			this._yearOfAssembly = +value;
+		}
+		else {
+			throw InvalidNumberError + " The valid value of the year is from 1000 to 9999";
+		}
 		lockObjectField(this, "_yearOfAssembly");
 	},
 
-	beep: function() { console.log(this._model + " BEEP!"); },
+	getSignal: function() { return this._model + " BEEP!"; },
 
-	about: function() {
+	getAbout: function() {
 		return "Car info:\n"
 			+ "\tNumber - " + this._number + "\n"
 			+ "\tModel - " + this._model + "\n"
@@ -154,12 +217,21 @@ let Car = {
 
 /* Passengers Car */
 
-let PassengersCar = Object.create(Car);
-PassengersCar.constructor = function(model, number, owner, weight, maxSpeed, mileage, wheelsAmount, yearOfAssembly, passengersSeatsAmount) {
+const PassengersCar = Object.create(Car);
+PassengersCar.constructor = function(
+	model,
+	number,
+	owner,
+	weight,
+	maxSpeed,
+	mileage,
+	wheelsAmount,
+	yearOfAssembly,
+	passengersSeatsAmount
+) {
 	Car.constructor.apply(this, arguments);
 	this._passengersSeatsAmount = passengersSeatsAmount || 0;
 	lockAllFields(this);
-	Object.seal(this);
 	return this;
 };
 
@@ -169,23 +241,37 @@ Object.defineProperty(PassengersCar, "passengersSeatsAmount", {
 	},
 	set: function (value) {
 		unlockObjectField(this, "_passengersSeatsAmount");
-		this._passengersSeatsAmount = !value ? this._passengersSeatsAmount : isNaN(+value) ? this._passengersSeatsAmount : value;
+		if (isNumeric(value) && value > 0) {
+			this._passengersSeatsAmount = +value;
+		}
+		else {
+			throw InvalidNumberError;
+		}
 		lockObjectField(this, "_passengersSeatsAmount");
 	}
 });
 
-PassengersCar.about = function() {
-	return Car.about.call(this) + "\tAmount of passengers seats - " + this._passengersSeatsAmount + "\n";
+PassengersCar.getAbout = function() {
+	return Car.getAbout.call(this) + "\tAmount of passengers seats - " + this._passengersSeatsAmount + "\n";
 };
 
 /* Autotruck */
 
-let Autotruck = Object.create(Car);
-Autotruck.constructor = function(model, number, owner, weight, maxSpeed, mileage, wheelsAmount, yearOfAssembly, maxCargoWeight) {
+const Autotruck = Object.create(Car);
+Autotruck.constructor = function(
+	model,
+	number,
+	owner,
+	weight,
+	maxSpeed,
+	mileage,
+	wheelsAmount,
+	yearOfAssembly,
+	maxCargoWeight
+) {
 	Car.constructor.apply(this, arguments);
 	this._maxCargoWeight = maxCargoWeight || 0;
 	lockAllFields(this);
-	Object.seal(this);
 	return this;
 };
 
@@ -195,23 +281,27 @@ Object.defineProperty(Autotruck, "maxCargoWeight", {
 	},
 	set: function (value) {
 		unlockObjectField(this, "_maxCargoWeight");
-		this._maxCargoWeight = !value ? this._maxCargoWeight : isNaN(+value) ? this._maxCargoWeight : +value;
+		if (isNumeric(value) && value > 0) {
+			this._maxCargoWeight = +value;
+		}
+		else {
+			throw InvalidNumberError;
+		}
 		lockObjectField(this, "_maxCargoWeight");
 	}
 });
 
-Autotruck.about = function() {
-	return Car.about.call(this) + "\tMaximum cargo weight - " + this._maxCargoWeight + "\n";
+Autotruck.getAbout = function() {
+	return Car.getAbout.call(this) + "\tMaximum cargo weight - " + this._maxCargoWeight + "\n";
 };
 
 /* SportCar */
 
-let SportCar = Object.create(Car);
+const SportCar = Object.create(Car);
 SportCar.constructor = function(model, number, owner, weight, maxSpeed, mileage, wheelsAmount, yearOfAssembly, racingType) {
 	Car.constructor.apply(this, arguments);
 	this._racingType = racingType || "unknown racing type";
 	lockAllFields(this);
-	Object.seal(this);
 	return this;
 };
 
@@ -221,52 +311,58 @@ Object.defineProperty(SportCar, "racingType", {
 	},
 	set: function (value) {
 		unlockObjectField(this, "_racingType");
-		this._racingType = !value ? this._racingType : /\W/.test(value) ? this._racingType : value;
+		if (!value) {
+			throw InvalidStringError;
+		}
+		else {
+			this._racingType = value + "";
+		}
 		lockObjectField(this, "_racingType");
 	}
 });
 
-SportCar.about = function() {
-	return Car.about.call(this) + "\tRacing type - " + this._racingType + "\n";
+SportCar.getAbout = function() {
+	return Car.getAbout.call(this) + "\tRacing type - " + this._racingType + "\n";
 };
+//#endregion
 
 /*
 	Objects declaration
 */
 
 let uazik = Object.create(PassengersCar).constructor("UAZ", "0000PB5", "Old Bobby", 3500, 160, 15000, 4, 1970, 5);
-uazik.beep();
-console.log(uazik.about());
+console.log(uazik.getSignal());
+console.log(uazik.getAbout());
 
 let truck = Object.create(Autotruck).constructor("Mersedes", "0550PB4", "Bobby Singer", 10000, 130, 75000, 10, 2012, 10000);
-truck.beep();
-console.log(truck.about());
+console.log(truck.getSignal());
+console.log(truck.getAbout());
 
-let paganiZondaF = Object.create(SportCar).constructor("Pagani", "6666PB6", "Gachimuchi", 2000, 400, 3000, 4, 2015, "LeMan");
-paganiZondaF.beep();
-console.log(paganiZondaF.about());
+let paganiZondaF = Object.create(SportCar).constructor("Pagani", "6666PB6", "Gachimuchi", 2000, 400, 3000, 4, 2015, "Le Man");
+console.log(paganiZondaF.getSignal());
+console.log(paganiZondaF.getAbout());
 
 //----------TESTING-----------
-// uazik.model = "wfwe wefwef";
+// uazik.model = "awd";
 // console.log(uazik.model);
 // uazik.number = "0000PB5";
 // console.log(uazik.number);
 // uazik.owner = "Old Bobby";
 // console.log(uazik.owner);
-// uazik.weight = 3500;
+// uazik.weight = 500;
 // console.log(uazik.weight);
 // uazik.maxSpeed = 160;
 // console.log(uazik.maxSpeed);
 // uazik.mileage = 15000;
 // console.log(uazik.mileage);
-// uazik.wheelsAmount = 4;
+// uazik.wheelsAmount = "120";
 // console.log(uazik.wheelsAmount);
-// uazik.yearOfAssembly = "1980";
+// uazik.yearOfAssembly = "20d00";
 // console.log(uazik.yearOfAssembly);
 // uazik.passengersSeatsAmount = 5;
 // console.log(uazik.passengersSeatsAmount);
 
-/* НЕПОНЯТНАЯ (уже более или менее понятная) ХРЕНЬ!!! В одном месте beep не считается функцией, в другом считается */
+/* НЕПОНЯТНАЯ (уже более или менее понятная) ХРЕНЬ!!! В одном месте getSignal не считается функцией, в другом считается */
 
 /* let Car = {
 	constructor: (model, owner) => {
@@ -274,11 +370,11 @@ console.log(paganiZondaF.about());
 		this.owner = owner;
 		return this;
 	},
-	beep: () => { return this.model + " BEEP!"; }
+	getSignal: () => { return this.model + " BEEP!"; }
 };
 
 let ef = Object.create(Car).constructor();
-// console.log(ef.beep());
+// console.log(ef.getSignal());
 
 let LightCar = Object.create(Car);
 LightCar.constructor = function(model, owner) {
@@ -287,4 +383,4 @@ LightCar.constructor = function(model, owner) {
 };
 
 let carObject = Object.create(LightCar).constructor("Mers", "John");
-console.log(carObject.beep()); */
+console.log(carObject.getSignal()); */
