0fb9741d6e17e2984152c86cfcb320434b870223
YegorFeoktistov <YegorFeoktistov@gmail.com>
2018-04-17 18:48:29 +0300

    script changes

--- Task4/main.js
+++ Task4/main.js
@@ -1,11 +1,7 @@
 const formActionTextInput = document.getElementById("form-action-text-input");
 const choiceTypeSelectInput = document.getElementById("choice-type-select-input");
-// const subscribeRadiobuttonInput = document.getElementById("subscribe-radiobutton-input");
-// const unsubscribeRadiobuttonInput = document.getElementById("unsubscribe-radiobutton-input");
-
 const radiobuttons = document.getElementsByName("choices");
 const radiobuttonsContainer = document.querySelector(".subscribe-form__radiobutton-input-section");
-
 const paragraphCheckboxInput = document.getElementById("paragraph-checkbox-input");
 
 formActionTextInput.addEventListener("change", function (e) {
@@ -16,20 +12,6 @@ choiceTypeSelectInput.addEventListener("change", function (e) {
 	localStorage.setItem("choiceTypeSelectInputValue", choiceTypeSelectInput.value);
 });
 
-// subscribeRadiobuttonInput.addEventListener("change", function (e) {
-// 	localStorage.setItem("subscribeRadiobuttonInputIsChecked", subscribeRadiobuttonInput.checked);
-// });
-
-// unsubscribeRadiobuttonInput.addEventListener("change", function (e) {
-// 	localStorage.setItem("unsubscribeRadiobuttonInputIsChecked", unsubscribeRadiobuttonInput.checked);
-// });
-
-// radiobuttonsClass.addEventListener("click", function (e) {
-// 	localStorage.setItem("subscribeRadiobuttonInputIsChecked", subscribeRadiobuttonInput.checked);
-// 	localStorage.setItem("unsubscribeRadiobuttonInputIsChecked", unsubscribeRadiobuttonInput.checked);
-// });
-
-
 radiobuttonsContainer.addEventListener("click", function (e) {
 	for (var i = 0; i < radiobuttons.length; i++) {
 		if (radiobuttons[i].checked) {
@@ -38,7 +20,6 @@ radiobuttonsContainer.addEventListener("click", function (e) {
 	}
 });
 
-
 paragraphCheckboxInput.addEventListener("change", function (e) {
 	localStorage.setItem("paragraphCheckboxInputIsChecked", paragraphCheckboxInput.checked);
 });
@@ -46,11 +27,7 @@ paragraphCheckboxInput.addEventListener("change", function (e) {
 const clearState = function () {
 	localStorage.removeItem("formActionTextInputValue");
 	localStorage.removeItem("choiceTypeSelectInputValue");
-	// localStorage.removeItem("subscribeRadiobuttonInputIsChecked");
-	// localStorage.removeItem("unsubscribeRadiobuttonInputIsChecked");
-
 	localStorage.removeItem("radiobuttonValue");
-
 	localStorage.removeItem("paragraphCheckboxInputIsChecked");
 };
 
@@ -61,9 +38,6 @@ const updateState = function () {
 	formActionTextInput.value = localStorage.getItem("formActionTextInputValue") || "";
 	choiceTypeSelectInput.value = localStorage.getItem("choiceTypeSelectInputValue");
 	document.querySelector("input[value=" + localStorage.getItem("radiobuttonValue") + "]").checked = true;
-
-	// subscribeRadiobuttonInput.checked = localStorage.getItem("subscribeRadiobuttonInputIsChecked") === "true" ? true : false;
-	// unsubscribeRadiobuttonInput.checked = localStorage.getItem("unsubscribeRadiobuttonInputIsChecked") === "true" ? true : false;
 	paragraphCheckboxInput.checked = localStorage.getItem("paragraphCheckboxInputIsChecked") === "true" ? true : false;
 };
 
